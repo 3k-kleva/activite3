95181997c4f86055ef9b92c4bc59e1eac5d29977
3k-kleva <koffi.kougblenou@etud.u-picardie.fr>
2019-06-16 22:14:38 +0000

    First commit

---
+++ src/main/java/GrilleImpl.java
@@ -0,0 +1,183 @@
+import java.util.Arrays;
+/**
+ * Implementation d'une grille .
+ */
+public class GrilleImpl implements Grille {
+  /** Déclaration de constante. */
+  private static final int NEUF = 9;
+  /** Tableau de caracteres à deux dimension .*/
+  public static char[][] grille = new char[][] {
+    {'.', '@', '@', '@', '3', '@', '@', '6', '2'},
+    {'@', '@', '@', '@', '7', '2', '@', '@', '1'},
+    {'2', '@', '@', '6', '@', '@', '8', '@', '@'},
+    {'@', '@', '@', '4', '5', '6', '3', '8', '7'},
+    {'6', '7', '4', '@', '@', '@', '2', '@', '@'},
+    {'5', '8', '@', '1', '@', '7', '6', '@', '4'},
+    {'@', '@', '6', '@', '@', '1', '@', '@', '3'},
+    {'4', '@', '@', '3', '8', '@', '@', '@', '@'},
+    {'7', '3', '@', '@', '6', '@', '@', '@', '@'}
+    };
+  /**
+   * @return largeur/hauteur de la grille 9 ou 16 .
+   */
+  public final int getDimension() {
+    return grille.length;
+  }
+  /**
+   * Test la presence de la valeur dans la colonne.
+   * @param y       position y dans la grille
+   * @param value       valeur à rechercher dans la grille
+   * @return vrai si la valeur est presente sinon faux
+   */
+  public static boolean checkColumn(final int y, final char value) {
+    for (int x = 0; x < NEUF; x++) {
+      if (grille[x][y] == value) {
+        return true;
+      }
+    }
+    return false;
+  }
+  /**
+   * Test la presence de la valeur sur la ligne.
+   * @param x       position x dans la grille
+   * @param value       valeur à rechercher dans la grille
+   * @return vrai si la valeur est presente sinon faux
+   */
+  public static boolean checkLine(final int x, final char value) {
+    for (int y = 0; y < NEUF; y++) {
+      if (grille[x][y] == value) {
+        return true;
+      }
+    }
+    return false;
+  }
+  /**
+   * Test la presence de la valeur dans le carré.
+   * @param x       position x dans la grille
+   * @param y       position y dans la grille
+   * @param value       valeur à rechercher dans la grille
+   * @return vrai si la valeur est presente sinon faux
+   */
+  public static boolean
+  checkSquare(final int x, final int y, final char value) {
+    int dimenssion = grille.length;
+    int leftpoint = 0;
+    int toppoint = 0;
+    final int q = 3;
+    final int dim = 9;
+    if (dimenssion == dim) {
+      leftpoint = q * (y / q);
+      toppoint = q * (x / q);
+    }
+    for (int c = leftpoint; c < leftpoint + q; c++) {
+      for (int l = toppoint; l < toppoint + q; l++) {
+        if (grille[l][c] == value) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+ /**
+  * Affecte une valeur dans la grille.
+  * @param x       position x dans la grille
+  * @param y       position y dans la grille
+  * @param value
+  *            valeur a mettre dans la case
+  * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
+  * @throw IllegalArgumentException si la valeur est interdite aux vues des
+  *        autres valeurs de la grille
+  * @throw IllegalArgumentException si value n'est pas un caractere autorise
+  *        ('1',...,'9')
+  */
+  public final void setValue(final int x, final int y, final char value) {
+    int dimenssion = grille.length;
+    boolean cont = false;
+    cont = Arrays.toString(POSSIBLE).contains(Character.toString(value));
+    if (cont == true) {
+    //verification
+    if ((x >= NEUF) || (x < 0)) {
+        throw new IllegalArgumentException("x est hors bornes (0-8)");
+    } else if ((y >= NEUF) || (y < 0)) {
+          throw new IllegalArgumentException("y est hors bornes (0-8)");
+        } else if ((!checkColumn(y, value)) && (!checkLine(x, value))
+            && (!checkSquare(x, y, value)) && !(grille[x][y] == EMPTY)) {
+            throw new IllegalArgumentException("la valeur est interdite "
+                     + "aux vues des autres valeurs de la grille");
+            } else {
+              grille[x][y] = value;
+            }
+    } else {
+        throw new IllegalArgumentException("La value n'est"
+                               + " pas un caractere autorise");
+      }
+  }
+ /**
+  * Recupere une valeur de la grille.
+  * @param x      position x dans la grille
+  * @param y      position y dans la grille
+  * @return valeur dans la case x,y
+  * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
+  */
+  public final char getValue(final int x, final int y) {
+    if ((x >= NEUF) || (x < 0)) {
+       throw new IllegalArgumentException("x est hors bornes (0-8)");
+    } else if ((y >= NEUF) || (y < 0)) {
+          throw new IllegalArgumentException("y est hors bornes (0-8)");
+        }
+    return grille[x][y];
+  }
+  /**
+   * Test si une grille est terminee.
+   * @return true si la grille est complete
+   */
+   public final boolean complete() {
+     int dimension = this.getDimension();
+     boolean controle = true;
+     for (int i = 0; i < dimension; i++) {
+        for (int j = 0; j < dimension; j++) {
+          if (grille[i][j] == EMPTY) {
+            controle = false;
+            continue;
+          }
+        }
+        if (controle == false) {
+          continue;
+        }
+     }
+     return controle;
+   }
+ /**
+  * Test si une valeur est possible dans la grille par rapport a ce qu'elle .
+  * contient deja
+  * @param x     position x dans la grille
+  * @param y     position y dans la grille
+  * @param value a mettre dans la case
+  * @throw IllegalArgumentException si x ou y sont hors bornes (0-8)
+  * @throw IllegalArgumentException si value n'est pas un caractere autorise
+  *        ('1',...,'9',..)
+  * @return true si c'est une valeur possible
+  */
+  public final boolean possible(final int x, final int y, final char value) {
+    boolean cont = false;
+    cont = Arrays.toString(POSSIBLE).contains(Character.toString(value));
+    if (cont == true) {
+      //verification
+      if ((x >= NEUF) || (x < 0)) {
+        throw new IllegalArgumentException("x est hors bornes (0-8)");
+      }
+      if ((y >= NEUF) || (y < 0)) {
+          throw new IllegalArgumentException("y est hors bornes (0-8)");
+      }
+      if ((!checkColumn(y, value)) && (!checkLine(x, value))
+                  && (!checkSquare(x, y, value)) && (grille[x][y] == EMPTY)) {
+            return true;
+      } else {
+             return false;
+         }
+    } else {
+        throw new IllegalArgumentException("La value n'est pas "
+        + "un caractere autorise");
+      }
+  }
+}
---
+++ src/test/java/GrilleImplTest.java
@@ -0,0 +1,142 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+public class GrilleImplTest{ 
+  static final char M = '.';
+  GrilleImpl grid = new GrilleImpl();
+  @Test
+  public void GetDimensionTest() {
+    assertEquals(9, grid.getDimension());	
+  }
+  
+  @Test
+  public void setValueTest() {
+    grid.setValue(2, 2, '9');
+  } 
+  /** Déclaration de l'exception. */
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+  /** Test de la methode division cas ou b vaut 0. */
+  @Test
+  public void setValueTest2() { 
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("x est hors bornes (0-8)");
+	grid.setValue(9, 3, '7');
+  }
+ 
+  @Test
+  public void setValueTest3() { 
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("y est hors bornes (0-8)");
+	grid.setValue(3, 9, '7');
+  }
+ 
+  @Test
+  public void setValueTest4() { 
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("value n'est pas un caractere autorise");
+	grid.setValue(3, 3, '-');
+  } 
+ 
+  @Test
+  public void setValueTest5() { 
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("la valeur est interdite aux vues des autres valeurs de la grille");
+	grid.setValue(0, 8, '9');
+  }  
+
+  @Test
+  public void getValueTest() {  
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("x est hors bornes (0-8)");
+	grid.getValue(9, 3);
+  } 
+  @Test
+  public void getValueTest1() {  
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("y est hors bornes (0-8)");
+	grid.getValue(3, 9);
+  } 
+  /**  
+  @Test
+  public void getValueTest3() {  
+    assertArrayEquals('6', grid.getValue(0, 7));	
+  } */
+  
+  @Test
+  public void possibleTest1() {  
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("x est hors bornes (0-8)");
+	grid.possible(9, 3, '8');
+  } 
+  @Test
+  public void possibleTest2() {  
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("y est hors bornes (0-8)");
+	grid.possible(3, 9, '8');
+  } 
+  @Test
+  public void possibleTest3() {  
+	thrown.expect(IllegalArgumentException.class);
+	thrown.expectMessage("La value n'est pas un caractere autorise");
+	grid.possible(3, 9, 'z');
+  }
+  
+  @Test
+  public void possibleTest4() {  
+    assertTrue(grid.possible(2, 2, '7'));
+  } 
+  @Test
+  public void possibleTest5() {
+    assertFalse(grid.possible(2, 2, '4'));
+  }
+  
+  @Test
+  public void checkColumnTest() {  
+    assertTrue(GrilleImpl.checkColumn(2, '6'));
+  }
+  @Test
+  public void checkColumnTest1() {  
+    assertFalse(GrilleImpl.checkColumn(2, '7'));
+  } 
+  
+  @Test
+  public void checkLineTest() {  
+    assertTrue(GrilleImpl.checkLine(2, '6'));
+  }
+/**  @Test
+  public void checkLineTest2() {  
+	//assertFalse(GrilleImpl.checkLine(2, '6'));
+	boolean bool = GrilleImpl.checkLine(2, '7');
+	assertFalse(bool);
+  } 
+  */
+  @Test
+  public void checkSquareTest() {  
+    assertTrue(GrilleImpl.checkSquare(2, 2, '2'));
+  }
+  @Test
+  public void checkSquareTest1() {  
+    assertFalse(GrilleImpl.checkSquare(2, 2, '7'));
+  } 
+  
+  @Test
+  public void completeTest() {  
+    grid.grille = new char[][]{
+    {'9', '4', '1', '5', '3', '8', '7', '6', '2'},
+    {'3', '6', '8', '9', '7', '2', '4', '5', '1'},
+    {'2', '5', '7', '6', '1', '4', '8', '3', '9'},
+    
+    {'1', '2', '9', '4', '5', '6', '3', '8', '7'},
+    {'6', '7', '4', '8', '9', '3', '2', '1', '5'},
+    {'5', '8', '3', '1', '2', '7', '6', '9', '4'},
+    
+    {'8', '9', '6', '7', '4', '1', '5', '2', '3'},
+    {'4', '1', '2', '3', '8', '5', '9', '7', '6'},
+    {'7', '3', '5', '2', '6', '9', '1', '4', '8'}
+	};  
+    assertTrue(grid.complete());	
+  }
+  
+}
