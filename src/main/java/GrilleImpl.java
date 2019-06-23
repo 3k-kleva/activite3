84146e0ebb983165e6509e6529b8f5a99d71b3ed
3k-kleva <koffi.kougblenou@etud.u-picardie.fr>
2019-06-23 14:30:54 +0000

    new update

--- src/main/java/GrilleImpl.java
+++ src/main/java/GrilleImpl.java
@@ -1,13 +1,23 @@
-import java.util.Arrays;
 /**
  * Implementation d'une grille .
  */
 public class GrilleImpl implements Grille {
   /** Déclaration de constante. */
   private static final int NEUF = 9;
+ /**
+  * Caractere de case vide.
+  */
+ public static final char EMPTY = '@';
+ /**
+  * Caractere possible a mettre dans la grille.
+  * pour une grille 9x9 : 1..9
+  * pour une grille 16x16: 0..9-a..f
+  */
+  private static final char[] POSSIBLE = new char[]
+  {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
   /** Tableau de caracteres à deux dimension .*/
-  public static char[][] grille = new char[][] {
-    {'.', '@', '@', '@', '3', '@', '@', '6', '2'},
+  private static final char[][] GRILLE_A_RESOUDRE = new char[][] {
+    {'@', '@', '@', '@', '3', '@', '@', '6', '2'},
     {'@', '@', '@', '@', '7', '2', '@', '@', '1'},
     {'2', '@', '@', '6', '@', '@', '8', '@', '@'},
     {'@', '@', '@', '4', '5', '6', '3', '8', '7'},
@@ -17,6 +27,49 @@ public class GrilleImpl implements Grille {
     {'4', '@', '@', '3', '8', '@', '@', '@', '@'},
     {'7', '3', '@', '@', '6', '@', '@', '@', '@'}
     };
+  /** Déclaration d'un exemple de grille. */
+  private char[][] grille;
+  /**
+   * Constructeur Grille .
+   */
+  public GrilleImpl() {
+    this.grille = new char[NEUF][NEUF];
+    for (int i = 0; i < NEUF; i++) {
+      for (int j = 0; j < NEUF; j++) {
+        this.grille[i][j] = GRILLE_A_RESOUDRE[i][j];
+      }
+    }
+  }
+  /**
+   * Constructeur Grille avec argument .
+   * @param tab est une grille
+   */
+  public GrilleImpl(final char[][] tab) {
+    this.grille = new char[NEUF][NEUF];
+    for (int i = 0; i < NEUF; i++) {
+      for (int j = 0; j < NEUF; j++) {
+        this.grille[i][j] = tab[i][j];
+      }
+    }
+  }
+  /**
+   * Accesseur de grille.
+   * @param x est entier
+   * @param y est un entier
+   * @return retourne la grile
+   */
+  public final char getGrille(final int x, final int y) {
+    return grille[x][y];
+  }
+  /**
+   * Mutateur de grille.
+   * @param x est entier
+   * @param y est un entier
+   * @param nouvGrille est un caractere
+   */
+  public final void setGrille(final int x, final int y, final char nouvGrille) {
+    grille[x][y] = nouvGrille;
+  }
   /**
    * @return largeur/hauteur de la grille 9 ou 16 .
    */
@@ -24,14 +77,27 @@ public class GrilleImpl implements Grille {
     return grille.length;
   }
   /**
+   * Test la presence de la valeur dans le tableau.
+   * @param value       valeur à rechercher dans le tableau
+   * @return vrai si la valeur est presente sinon faux
+   */
+  public final boolean checkCaractere(final char value) {
+    for (int x = 0; x < NEUF; x++) {
+      if (this.POSSIBLE[x] == value) {
+        return true;
+      }
+    }
+    return false;
+  }
+  /**
    * Test la presence de la valeur dans la colonne.
    * @param y       position y dans la grille
    * @param value       valeur à rechercher dans la grille
    * @return vrai si la valeur est presente sinon faux
    */
-  public static boolean checkColumn(final int y, final char value) {
+  public final boolean checkColumn(final int y, final char value) {
     for (int x = 0; x < NEUF; x++) {
-      if (grille[x][y] == value) {
+      if (this.grille[x][y] == value) {
         return true;
       }
     }
@@ -43,9 +109,9 @@ public class GrilleImpl implements Grille {
    * @param value       valeur à rechercher dans la grille
    * @return vrai si la valeur est presente sinon faux
    */
-  public static boolean checkLine(final int x, final char value) {
+  public final boolean checkLine(final int x, final char value) {
     for (int y = 0; y < NEUF; y++) {
-      if (grille[x][y] == value) {
+      if (this.grille[x][y] == value) {
         return true;
       }
     }
@@ -58,7 +124,7 @@ public class GrilleImpl implements Grille {
    * @param value       valeur à rechercher dans la grille
    * @return vrai si la valeur est presente sinon faux
    */
-  public static boolean
+  public final boolean
   checkSquare(final int x, final int y, final char value) {
     int dimenssion = grille.length;
     int leftpoint = 0;
@@ -91,10 +157,10 @@ public class GrilleImpl implements Grille {
   *        ('1',...,'9')
   */
   public final void setValue(final int x, final int y, final char value) {
-    int dimenssion = grille.length;
-    boolean cont = false;
-    cont = Arrays.toString(POSSIBLE).contains(Character.toString(value));
-    if (cont == true) {
+    //int dimenssion = grille.length;
+    //boolean cont = false;
+    //cont = Arrays.toString(POSSIBLE).contains(Character.toString(value));
+    if (checkCaractere(value)) {
     //verification
     if ((x >= NEUF) || (x < 0)) {
         throw new IllegalArgumentException("x est hors bornes (0-8)");
@@ -132,20 +198,14 @@ public class GrilleImpl implements Grille {
    * @return true si la grille est complete
    */
    public final boolean complete() {
-     int dimension = this.getDimension();
-     boolean controle = true;
-     for (int i = 0; i < dimension; i++) {
-        for (int j = 0; j < dimension; j++) {
+     for (int i = 0; i < NEUF; i++) {
+        for (int j = 0; j < NEUF; j++) {
           if (grille[i][j] == EMPTY) {
-            controle = false;
-            continue;
+            return false;
           }
         }
-        if (controle == false) {
-          continue;
-        }
      }
-     return controle;
+     return true;
    }
  /**
   * Test si une valeur est possible dans la grille par rapport a ce qu'elle .
@@ -159,9 +219,9 @@ public class GrilleImpl implements Grille {
   * @return true si c'est une valeur possible
   */
   public final boolean possible(final int x, final int y, final char value) {
-    boolean cont = false;
-    cont = Arrays.toString(POSSIBLE).contains(Character.toString(value));
-    if (cont == true) {
+    //boolean cont = false;
+    //cont = Arrays.toString(POSSIBLE).contains(Character.toString(value));
+    if (checkCaractere(value)) {
       //verification
       if ((x >= NEUF) || (x < 0)) {
         throw new IllegalArgumentException("x est hors bornes (0-8)");
@@ -169,12 +229,8 @@ public class GrilleImpl implements Grille {
       if ((y >= NEUF) || (y < 0)) {
           throw new IllegalArgumentException("y est hors bornes (0-8)");
       }
-      if ((!checkColumn(y, value)) && (!checkLine(x, value))
-                  && (!checkSquare(x, y, value)) && (grille[x][y] == EMPTY)) {
-            return true;
-      } else {
-             return false;
-         }
+      return ((!checkColumn(y, value)) && (!checkLine(x, value))
+                  && (!checkSquare(x, y, value)));
     } else {
         throw new IllegalArgumentException("La value n'est pas "
         + "un caractere autorise");
---
+++ src/main/java/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * Javadoc du package.
+ */
